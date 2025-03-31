b2df40c67e59d803e2988a7a5b6878e142d79f13
weii0705 <ivyhsu520@gmail.com>
2025-03-31 20:50:46 +0800

    Add files via upload

---
+++ web_project/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+document.querySelector("html").onclick = function() {
+    alert("Ouch! Stop poking me!");
+}
+alert("hello");
